2c0982c2d80a6ec34797327e2b14c07963246cb3
Juan Jose Lopez Villar <juanjose.lopezvillar@amaris.com>
2018-01-30 10:31:38 +0100

    next step

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,11 +3,27 @@ export class Store {
   private reducers: { [key: string]: Function };
   private state: { [key: string]: any };
 
-  constructor(reducers = {}, initalState = {}) {
-    this.state = initalState;
+  constructor(reducers = {}, initialState = {}) {
+    this.reducers = reducers;
+    this.state = this.reduce(initialState, {});
   }
 
   get value() {
     return this.state;
+  }
+
+  dispatch(action) {
+    this.state = this.reduce(this.state, action);
+  }
+
+  private reduce(state, action) {
+    const newState = {};
+    for (const prop in this.reducers) {
+      if (this.reducers.hasOwnProperty(prop)) {
+        newState[prop] = this.reducers[prop](state[prop], action);
+
+      }
+    }
+    return newState;
   }
 }
